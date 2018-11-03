fde1cbb6d92a54807db0936878477e1794bb1d73
Kyle Steinle <kyledavid022@gmail.com>
2018-11-02 17:09:08 -0700

    added mongoose and middleware

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', router);
 
 app.listen(3000, function () {
-console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!')
 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
-import express from 'express';
-let router = express.Router();
+import express from 'express'
+import mongoose from 'mongoose'
+const db = mongoose.connect('mongodb://poop:poopman1@ds249583.mlab.com:49583/express-rest')
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
+let router = express.Router()
 
-res.send('hello DAWGZ');
-});
+router.use('/rsvp', (req, res, next) => {
+  next()
+})
+router.route('/rsvp')
 
+router.use('/confirm', (req, res, next) => {
+  next()
+})
+router.route('/confirm')
 
-export default router;
+export default router
