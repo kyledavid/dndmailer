b6dd3e0639801d0183d3cd1e1f32e76bac79de14
Kyle Steinle <kyledavid022@gmail.com>
2018-11-02 20:00:52 -0700

    added pseudocode for routes and middleware

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,28 @@ const db = mongoose.connect('mongodb://poop:poopman1@ds249583.mlab.com:49583/exp
 
 let router = express.Router()
 
-router.use('/rsvp', (req, res, next) => {
-  next()
-})
-router.route('/rsvp')
+const rsvpMware = {
+  checkValues: 'fn that checks if correct values are in req body',
+  checkTime: 'fn that checks if its still early enough to create RSVP',
+  checkExisting: 'fn to check if there is current RSVP',
+}
 
-router.use('/confirm', (req, res, next) => {
-  next()
+router.route('/rsvp')
+.post([rsvpMware.checkValues, rsvpMware.checkTime, rsvpMware.checkExisting], (req, res) => {
+  // Saves RSVP to db
+  // Calls email function
 })
+
+const confirmMware = {
+  checkValues: 'fn that checks if correct values are in req body',
+  checkTime: 'fn to check if still before cutoff time',
+  checkExisting: 'fn to check if name is currently in the db',
+}
+
 router.route('/confirm')
+.post([confirmMware.checkValues, confirmMware.checkTime, confirmMware.checkExisting], (req,res) => {
+  // Saves RSVP
+  // Calls function to check if game threshhold has been reached, will then send email
+})
 
 export default router
